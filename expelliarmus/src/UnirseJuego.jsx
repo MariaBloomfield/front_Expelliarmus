9316ec3231d40c0756e6c23904e17b22baf8c090
Maria Bloomfield <mbloomfield@uc.cl>
2023-06-29 11:44:52 -0400

    game

--- expelliarmus/src/UnirseJuego.jsx
+++ expelliarmus/src/UnirseJuego.jsx
@@ -4,13 +4,16 @@ import { AuthContext } from "./auth/AuthContext";
 import './UnirseJuego.css';
 
 export default function UnirseJuego() {
-    const { user } = useContext(AuthContext);
+    const { user, gameId, setGameId } = useContext(AuthContext);
     const rollDice = () => {
-        axios.post(`${import.meta.env.VITE_BACKEND_URL}/game`, { user: user })
+        axios.post(`${import.meta.env.VITE_BACKEND_URL}/game`, { user: user, gameId: gameId})
         .then((response) => {
             // Manejar la respuesta del backend, si es necesario
             console.log(response.data);
             console.log("HOLA");
+            const { gameId } = response.data; // Obtener el gameId de la respuesta
+            console.log("gameId: ", gameId);
+            setGameId(gameId);
         })
         .catch((error) => {
             // Manejar el error en caso de que ocurra
--- expelliarmus/src/auth/AuthProvider.jsx
+++ expelliarmus/src/auth/AuthProvider.jsx
@@ -4,10 +4,12 @@ import { AuthContext } from "./AuthContext";
 function AuthProvider({ children }) {
     const [token, setToken] = useState(localStorage.getItem('token') || null);
     const [user, setUser] = useState(localStorage.getItem('user') || null);
+    const [gameId, setGameId] = useState(localStorage.getItem('gameId') || null);
 
     function logout() {
         setToken(null);
         setUser(null);
+        setGameId(null);
     }
 
     useEffect(() => {
@@ -18,8 +20,12 @@ function AuthProvider({ children }) {
         localStorage.setItem('userId', JSON.stringify(user));
     });
 
+    useEffect(() => {
+        localStorage.setItem('gameId', JSON.stringify(gameId));
+    });
+
     return (
-        <AuthContext.Provider value={{ token, setToken, logout, user, setUser }}>
+        <AuthContext.Provider value={{ token, setToken, logout, user, setUser, gameId, setGameId }}>
             {children}
         </AuthContext.Provider>
     );
